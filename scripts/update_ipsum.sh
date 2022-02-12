694b912cf9ae14ebaff7e0fc36917b7d1bac57de
fckpgn <arsen123456beeos@gmail.com>
2022-02-12 13:36:29 +0000

    autoupdate

--- scripts/update_ipsum.sh
+++ scripts/update_ipsum.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+git pull
 curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | cut -f 1 > ipsum.ipset
+git commit -a -m "autoupdate"
+git push
 echo Done
