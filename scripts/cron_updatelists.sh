5491fca6c84e4a99a7e3f891469492efb37e688b
fckpgn <arsen123456beeos@gmail.com>
2022-02-12 15:15:10 +0000

    update

--- scripts/cron_updatelists.sh
+++ scripts/cron_updatelists.sh
@@ -2,13 +2,14 @@
 
 #cron job sample for ubuntu with installed and working iptables
 #
-#packages: iptables iptables-persistent ipset ipset-persistent git curl
+#packages: iptables iptables-persistent ipset ipset-persistent git curl cron
 #
 #ipset persistent file path can be different, check it, for my system it is /etc/iptables/ipsets
 #
 #IPSET rules should be created:
 #ipset create ipsum -exist hash:net family inet hashsize 32768 maxelem 200000
 #ipset create badip_detected -exist hash:net family inet hashsize 32768 maxelem 200000
+#ipset save > /etc/iptables/ipsets
 #
 #IPTABLES rules should be created
 #-A INPUT -s YOURIP -p tcp -m tcp --dport 22 -j ACCEPT
@@ -20,11 +21,11 @@
 #cd /root
 #git clone https://github.com/fckpgn/blacklists.git
 #crontab -e
-#55 * * * * /root/blacklists/scripts/cron_updatelists.sh >/dev/null 2>&1
+#55 * * * * /root/blacklists/scripts/cron_updatelists.sh
 #
 #
 curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | cut -f 1 > /root/blacklists/ipsum.ipset
-curl --compressed https://raw.githubusercontent.com/fckpgn/blacklists/main/badip_detected.ipset > /root/blacklists/badip_detected.ipset
+curl --compressed https://raw.githubusercontent.com/fckpgn/blacklists/main/badip_detected.ipset 2>/dev/null > /root/blacklists/badip_detected.ipset
 
 ipset -q flush ipsum
 for ip in $(cat /root/blacklists/ipsum.ipset); do ipset add ipsum $ip; done
